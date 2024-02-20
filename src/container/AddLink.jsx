3a812cba5644e1ae98b61065bcf92d27f4ca636a
Pedro Alex <pedroalex663@gmail.com>
2024-02-20 11:49:32 -0300

    add-toCopyClickBoard

--- src/container/AddLink.jsx
+++ src/container/AddLink.jsx
@@ -3,8 +3,11 @@ import { Container } from "./addLink";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function AddLinks() {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("https://leave-message/m/p41h90r3?s=2");
 
+  function teste(){
+    console.log(setInputValue(inputValue));
+  }
 
   return (
     <Container className="">
@@ -16,12 +19,11 @@ function AddLinks() {
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          placeholder="https://leave-message/m/p41h90r3?s=2"
           readOnly
         />
 
         <CopyToClipboard text={inputValue}>
-          <button>Click to Copy</button>
+          <button onClick={teste}>Click to Copy</button>
         </CopyToClipboard>
         <a className="instagram">
           Add to Story
--- src/container/addLink.js
+++ src/container/addLink.js
@@ -1,12 +1,59 @@
 import styled from "styled-components";
 
-import Facebook from "../assets/messenger.svg";
-import Whatsapp from "../assets/whatsapp.svg";
-import Instagram from "../assets/instagram.svg";
-import Copy from "../assets/copy.svg";
-import Check from "../assets/check.svg";
 
 export const Container = styled.div`
-background-color: red;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
 
+  .information-box {
+    width: 418px;
+    height: 479px;
+    border: 1px solid #000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 15px;
+    h1 {
+      color: #000;
+      font-size: 15px;
+      font-weight: 500;
+      max-width: 380px;
+    }
+    input {
+      width: 380px;
+      height: 40px;
+      border: 2px solid #db7093;
+      color: #6c757d;
+      font-size: 15px;
+      font-weight: 500;
+      text-align: center;
+    }
+    button {
+      background-position: 100px;
+      background-repeat: no-repeat;
+      background-color: #6c757d;
+      width: 380px;
+      height: 40px;
+      border: 1px solid #000;
+      color: #fff;
+      font-size: 20px;
+      font-weight: 500;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &:hover{
+        opacity: 0.9;
+        transition: .5s ease;
+      }
+      &:active{
+        scale: .9.9;
+
+      }
+    }
+
+  }
 `;
